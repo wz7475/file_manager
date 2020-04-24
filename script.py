41b077db6c94fb88347d116c227fd00208ba570b
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-04-24 17:48:41 +0200

    3

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import os
-list_dirs_all = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", "pozostale"]
-list_dirs_selected = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", "pozostale"]
+name_rest_dir = "pozostale"
+list_dirs_all = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", name_rest_dir]
+list_dirs_selected = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", name_rest_dir]
 list_dirs_types = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane"]
 
 ext_dict = {
@@ -19,18 +20,20 @@ ext_dict = {
 """ print("drag path")
 path = input() 
 """
-path = "c:\\Users\\wojte\\Desktop"
+path = "c:\\Users\\wojte\\Desktop\\test"
 os.chdir(path)
 
 for i in os.listdir():
     if os.path.isfile(i):
         extension = os.path.splitext(i)[1]
         if extension not in ext_dict.keys():
-            os.rename(i, os.path.join(path, "pozostale"))
+            os.rename(i, os.path.join(path, name_rest_dir))
             continue
         dir_name = ext_dict[extension]
         if not os.path.exists(os.path.join(path, dir_name)):
             os.mkdir(dir_name)
         path_to_dir = os.path.join(path, dir_name)
         os.rename(i, os.path.join(path_to_dir, i))
+    else:
+        pass
 #print("\nend of script\n")
---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+name_rest_dir = "pozostale"
+path = "c:\\Users\\wojte\\Desktop\\test2"
+os.chdir(path)
+
+path_rest = os.path.join(path, name_rest_dir)
+
+for i in os.listdir():
+    if os.path.isdir(i):
+        for j in os.walk(i):
+            path_current_rest = os.path.join(path_rest, j[0])
+            path_current = os.path.join(path, j[0])
+            if not os.path.exists(path_current_rest):
+                os.mkdir(path_current_rest)
+            else:
+                os.mkdir(os.path.join(path_rest, "new"))
+                
