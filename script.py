1991a602490192bef2c29d6f6145a5dc474b6a94
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-04-24 20:46:35 +0200

    6

--- script.py
+++ script.py
@@ -1,21 +1,24 @@
 import os
+import shutil
 from datetime import datetime
+import json
 name_rest_dir = "pozostale"
 list_for_change = [".jpg", ".png"]
 """ list_dirs_selected = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", name_rest_dir]
 list_dirs_types = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane"] """
 
 ext_dict = {
-".docx" : "word",
-".xlsx" : "excel",
-".pptx" : "power_point",
-".pdf" : "pdf",
-".png" : "zdjecia",
-".jpg" : "zdjecia",
-".gif" : "zdjecia",
-".zip" : "skompresowane",
-".rar" : "skompresowane"
+    ".docx": "word",
+    ".xlsx": "excel",
+    ".pptx": "power_point",
+    ".pdf": "pdf",
+    ".png": "zdjecia",
+    ".jpg": "zdjecia",
+    ".gif": "zdjecia",
+    ".zip": "skompresowane",
+    ".rar": "skompresowane"
 }
+
 
 def name_changer(path):
     dir_name, file_full_name = os.path.split(path)
@@ -26,12 +29,56 @@ def name_changer(path):
     str_time = str(time.strftime("%Y-%m-%d %Hh %Mmin %Ss"))
     new_name = str_time + ext_name
     return new_name
-    #return os.path.join(dir_name, new_name)
+    # return os.path.join(dir_name, new_name)
 
-""" print("drag path")
-path = input() 
-"""
-path = "c:\\Users\\wojte\\Desktop\\test"
+
+def delete_dir():
+    name = input("Podaj nazwę folderu - uwaga usuwasz całą jego zawartość:\n")
+    if os.path.exists(os.path.join(os.getcwd(), name)):
+        shutil.rmtree(os.path.join(os.getcwd(), name), ignore_errors=True)
+
+
+def list_dirs():
+    for i in ext_dict.values():
+        print(i)
+
+
+def list_ext():
+    for i in ext_dict.keys():
+        print(i)
+
+def ext_checker(name):
+    if name[0] != ".":
+        return False
+    for i in name[1:]:
+        if ord(i) not in range(97, 123):
+            return False
+    return True
+
+def name_checker(name):
+    for i in name:
+        if i in ["/", "\\", ":", "*", "?", "<", ">"]:
+            return False
+    return True
+
+def add_ext():
+    ext = input("Podaj nazwe rozszerzenia np. .txt\n")
+    while not ext_checker(ext):
+        ext = input("Podaj nazwe rozszerzenia, pamiętaj o poprawności np. .txt\n")
+    if ext in ext_dict.keys():
+        print("Rozszerzenie już wspierane")
+        return
+    folder = input("Podaj nazwe folderu")
+    while not name_checker(folder):
+        folder = input("Podaj nazwe folderu, pamiętaj o poprawnośc")
+    ext_dict[ext] = folder
+    
+
+
+print("drag path")
+path = input()
+
+#path = "c:\\Users\\wojte\\Desktop\\test"
 os.chdir(path)
 
 for i in os.listdir():
@@ -51,17 +98,19 @@ for i in os.listdir():
                 os.rename(i, os.path.join(path_to_dir, i))
         except FileExistsError:
             if extension in list_for_change:
-                file_name_whole = os.path.basename(os.path.join(path, name_changer(i)))
+                file_name_whole = os.path.basename(
+                    os.path.join(path, name_changer(i)))
             else:
                 file_name_whole = os.path.basename(os.path.join(path, i))
-            
+
             file_name_base, file_name_ext = os.path.splitext(file_name_whole)
             file_name_base += "#"
             file_name_new = file_name_base + file_name_ext
-            
+
             dir_name = ext_dict[extension]
             path_rest = os.path.join(path, dir_name)
             if not os.path.exists(os.path.join(path, dir_name)):
                 os.mkdir(dir_name)
             path_to_dir = os.path.join(path, dir_name)
-            os.rename(os.path.join(path, i), os.path.join(path_rest, file_name_new))
+            os.rename(os.path.join(path, i), os.path.join(
+                path_rest, file_name_new))
