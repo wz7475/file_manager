530477fb16fdedfe7acfa91154a324bcb180ee50
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-04-24 19:24:41 +0200

    5

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
 from datetime import datetime
 name_rest_dir = "pozostale"
-list_dirs_all = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", name_rest_dir]
-list_dirs_selected = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", name_rest_dir]
-list_dirs_types = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane"]
+list_for_change = [".jpg", ".png"]
+""" list_dirs_selected = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane", name_rest_dir]
+list_dirs_types = ["word", "excel", "powerpoint", "pdf", "zdjecia", "skompresowane"] """
 
 ext_dict = {
 ".docx" : "word",
@@ -18,8 +18,15 @@ ext_dict = {
 }
 
 def name_changer(path):
+    dir_name, file_full_name = os.path.split(path)
     name = os.path.basename(path)
     file_name, ext_name = os.path.splitext(name)
+    mod_time = os.stat(path).st_mtime
+    time = datetime.fromtimestamp(mod_time)
+    str_time = str(time.strftime("%Y-%m-%d %Hh %Mmin %Ss"))
+    new_name = str_time + ext_name
+    return new_name
+    #return os.path.join(dir_name, new_name)
 
 """ print("drag path")
 path = input() 
@@ -38,10 +45,16 @@ for i in os.listdir():
             if not os.path.exists(os.path.join(path, dir_name)):
                 os.mkdir(dir_name)
             path_to_dir = os.path.join(path, dir_name)
-            os.rename(i, os.path.join(path_to_dir, i))
+            if extension in list_for_change:
+                os.rename(i, os.path.join(path_to_dir, name_changer(i)))
+            else:
+                os.rename(i, os.path.join(path_to_dir, i))
         except FileExistsError:
+            if extension in list_for_change:
+                file_name_whole = os.path.basename(os.path.join(path, name_changer(i)))
+            else:
+                file_name_whole = os.path.basename(os.path.join(path, i))
             
-            file_name_whole = os.path.basename(os.path.join(path, i))
             file_name_base, file_name_ext = os.path.splitext(file_name_whole)
             file_name_base += "#"
             file_name_new = file_name_base + file_name_ext
@@ -52,6 +65,3 @@ for i in os.listdir():
                 os.mkdir(dir_name)
             path_to_dir = os.path.join(path, dir_name)
             os.rename(os.path.join(path, i), os.path.join(path_rest, file_name_new))
-        else:
-            pass
-#print("\nend of script\n")
