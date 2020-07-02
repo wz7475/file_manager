77d14fb57a1535afedd51022a191e6656059c69a
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-07-02 13:47:55 +0200

    11

--- test_script.py
+++ test_script.py
@@ -1,10 +1,28 @@
 import unittest
 import script
 import os
+import shutil
 
 class Testscript(unittest.TestCase):
 
-    def setUp(self):
+    """ def setUp(self):
         with open(os.path.join(script.path, "test_file.txt"), "w") as target:
             pass
         
+    def tearDown(self):
+        os.remove(os.path.join(script.path, "test_file.txt")) """
+
+    @classmethod
+    def setUpClass(cls):
+        with open(os.path.join(script.path, "test_file.txt"), "w") as target:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(os.path.join(script.path, "test_file.txt"))
+
+    def test_for_test(self):
+        self.assertEqual(True, os.path.exists(os.path.join(script.path, "test_file.txt")))
+
+if __name__ == "__main__":
+    unittest.main()
